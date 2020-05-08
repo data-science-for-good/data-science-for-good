8469f8f7dcc683cb93800bf49883458e6da5337f
Paul Anderson <pauleanderson@gmail.com>
2020-05-08 15:39:31 -0700

    update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Why are we creating this organization?",
+        "excerpt":"This is a personal question for each member in our organization, but let me attempt to answer with my own motivation. I know that I am who I am today only because of the people who have helped shape and guide me over many years and through many hours of...","categories": ["blog"],
+        "tags": ["Jekyll","update"],
+        "url": "http://localhost:4000/blog/why-create/",
+        "teaser": null
+      }]
